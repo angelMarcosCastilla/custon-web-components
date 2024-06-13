698b67c2b44409b01e10eea1e43eb7e2a559201c
angelMarcosCastilla <ajmarcosc15@gmail.com>
2024-06-13 14:40:06 -0500

    [TimeAgo - fix]: error in name

--- web/public/timeAgo.js
+++ web/public/timeAgo.js
@@ -1,4 +1,4 @@
-const TIME_AGO_NAME = "an-timeago"
+const TIME_AGO_NAME = "an-time-ago"
 class TimeAgo extends HTMLElement {
   constructor() {
     super();
@@ -40,17 +40,13 @@ class TimeAgo extends HTMLElement {
   render() {
     const date = new Date(this.getAttribute('datetime') || Date.now());
     const languaje = this.getAttribute('languaje') || 'es';
-    const $span = document.createElement('span'); 
+    const $span = document.createElement('time'); 
     const { value, unit } = TimeAgo.timeAgoFormat(date);
-    const rtf = new Intl.RelativeTimeFormat(languaje, {
-      localeMatcher: 'best fit',
-      numeric: 'always',
-      style: 'long',
-    })
+    const rtf = new Intl.RelativeTimeFormat(languaje);
     $span.textContent = rtf.format(value, unit);
     $span.setAttribute('title', TimeAgo.format(date));
     this._shadowRoot.appendChild($span);
   }
 }
 
-customElements.define('time-ago', TIME_AGO_NAME);
+window.customElements.define(TIME_AGO_NAME, TimeAgo);
