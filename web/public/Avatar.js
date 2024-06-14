68889d000a529182b05c115888357049fb46957e
angelMarcosCastilla <ajmarcosc15@gmail.com>
2024-06-14 09:10:07 -0500

    [Avatar - fix]: add svg in empty avatar

--- web/public/Avatar.js
+++ web/public/Avatar.js
@@ -131,7 +131,7 @@ class Avatar extends HTMLElement {
   showinitial(show) {
     if (show) {
       this.$initials.style.display = "grid";
-      this.$initials.textContent = this.getInitials() || "-";
+      this.$initials.innerHTML = this.getInitials() || '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>';
       this.$avatarImage.style.display = "none";
     } else {
       this.$avatarImage.style.display = "block";
